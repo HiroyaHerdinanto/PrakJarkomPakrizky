0a37e2e428b497cf9fcaf1611ac90d0b55c754e3
HiroyaHerdinanto <Herdinantohiroya@gmail.com>
2025-09-05 16:04:41 +0700

    Run  "herdinantohiroya@gmail.com"   git config --global user.name "HiroyaHerdinanto"
    Run
    
    "herdinantohiroya@gmail.com"
      git config --global user.name "HiroyaHerdinanto"

---
+++ Week 3/CLientMO/Form1.cs
@@ -0,0 +1,117 @@
+﻿//Hiroya Herdinanto
+//5223600022
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CLientMO
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client;
+        Thread receiveThread;
+        bool running = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Tambahkan PictureBox manual
+            pictureBox1 = new PictureBox();
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(pictureBox1);
+
+            ConnectToServer();
+        }
+
+        // deklarasi di bawah class
+        private PictureBox pictureBox1;
+
+        private void ConnectToServer()
+        {
+            try
+            {
+                client = new TcpClient("127.0.0.1", 5000); // connect ke server lokal
+                receiveThread = new Thread(ReceiveFrames);
+                receiveThread.IsBackground = true;
+                receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal connect ke server: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReceiveFrames()
+        {
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                BinaryReader reader = new BinaryReader(stream);
+
+                while (running)
+                {
+                    // Baca panjang data (int32 = 4 byte)
+                    int length = reader.ReadInt32();
+
+                    // Pastikan panjang valid
+                    if (length <= 0) continue;
+
+                    // Baca data frame
+                    byte[] data = reader.ReadBytes(length);
+
+                    // Convert byte[] ke Image
+                    using (MemoryStream ms = new MemoryStream(data))
+                    {
+                        Image frame = Image.FromStream(ms);
+
+                        // Update UI di thread utama
+                        if (pictureBox1.InvokeRequired)
+                        {
+                            pictureBox1.Invoke((MethodInvoker)delegate
+                            {
+                                pictureBox1.Image?.Dispose(); // buang gambar lama biar ga leak
+                                pictureBox1.Image = new Bitmap(frame);
+                            });
+                        }
+                        else
+                        {
+                            pictureBox1.Image?.Dispose();
+                            pictureBox1.Image = new Bitmap(frame);
+                        }
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                // Server disconnect
+                if (running)
+                {
+                    MessageBox.Show("Koneksi ke server terputus.",
+                                    "Disconnected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saat menerima frame: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            running = false;
+            try
+            {
+                client?.Close();
+            }
+            catch { }
+            base.OnFormClosing(e);
+        }
+    }
+}
---
+++ Week 3/MovingObject/Form1.cs
@@ -0,0 +1,132 @@
+﻿//Hiroya Herdinanto
+//5223600022
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MovingObject
+{
+    public partial class Form1 : Form
+    {
+        // MovingObject variables
+        Pen red = new Pen(Color.Red);
+        Rectangle rect = new Rectangle(20, 20, 30, 30);
+        SolidBrush fillBlue = new SolidBrush(Color.Blue);
+        int slide = 10;
+
+        // Socket server
+        TcpListener server;
+        List<TcpClient> clients = new List<TcpClient>();
+        bool running = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Timer setup
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+
+            // Start server
+            StartServer();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            back();
+            rect.X += slide;
+            Invalidate();
+
+            // Push frame ke client
+            PushFrame();
+        }
+
+        private void back()
+        {
+            if (rect.X >= this.Width - rect.Width * 2)
+                slide = -10;
+            else if (rect.X <= rect.Width / 2)
+                slide = 10;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+
+            g.DrawRectangle(red, rect);
+            g.FillRectangle(fillBlue, rect);
+        }
+
+        private void StartServer()
+        {
+            server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            Console.WriteLine("Server started on port 5000");
+
+            Thread acceptThread = new Thread(() =>
+            {
+                while (running)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    lock (clients)
+                    {
+                        clients.Add(client);
+                    }
+                    Console.WriteLine("Client connected!");
+                }
+            });
+            acceptThread.IsBackground = true;
+            acceptThread.Start();
+        }
+
+        private void PushFrame()
+        {
+            Bitmap bmp = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+                g.DrawRectangle(red, rect);
+                g.FillRectangle(fillBlue, rect);
+            }
+
+            byte[] imgData;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                imgData = ms.ToArray();
+            }
+
+            lock (clients)
+            {
+                foreach (var client in clients.ToArray())
+                {
+                    try
+                    {
+                        NetworkStream stream = client.GetStream();
+                        BinaryWriter writer = new BinaryWriter(stream);
+                        writer.Write(imgData.Length);
+                        writer.Write(imgData);
+                        writer.Flush();
+                    }
+                    catch
+                    {
+                        clients.Remove(client); // remove disconnected clients
+                    }
+                }
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            running = false;
+            server.Stop();
+            base.OnFormClosing(e);
+        }
+    }
+}
